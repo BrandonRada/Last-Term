3d3f86586e72826b77d18ade4b78ec8135a1bdbf
BrandonRada <brandonn.rada@gmail.com>
2025-02-02 21:35:13 -0800

    edits

--- COMP4537/labs/3/lang/messages/en/user.js
+++ COMP4537/labs/3/lang/messages/en/user.js
@@ -1,5 +1,7 @@
 //Unused for lab 1.
 
-export const messages = {
-    greetingMessage: "Hello %1, What a beautiful day."
+module.exports = {
+    messages: {
+        greetingMessage: "Hello %1, What a beautiful day."
+    }
 };
