ab764209ce0931bb2a50f79e651db7e99eaf508e
Brandon Rada <brandonn.rada@gmail.com>
2025-02-13 15:12:10 -0800

    setup files and the code.

---
+++ COMP4537/labs/5/lang/messages/en/responseMsg.js
@@ -0,0 +1,11 @@
+module.exports =
+{
+    defUndefined: 'definition is undefined!',
+    wordUndefinded: 'word is undefined!',
+    noNumbers: 'numbers are not allowed!',
+    alreadyExists: `Warning! '%1' already exists!`,
+    newEntry: `New entry recorded: '%1: %2'`,
+    notFound: `Word '%1' not found!`,
+    methodNotAllowed: '%1 Method Not Allowed',
+    four04: '404 Not Found',
+};
---
+++ COMP4537/labs/5/lang/messages/en/user.js
@@ -0,0 +1,10 @@
+export const messages = {
+    missingWordOrDefinition: "Error: Word and definition are required.",
+    invalidWord: "Error: Word can only contain letters, spaces, and hyphens.",
+    invalidDefinition: "Error: Definition can only contain letters, spaces, periods, commas, and hyphens.",
+    missingSearchWord: "Please enter a word to search.",
+    searchResult: "%1: %2",
+    requestSuccess: "Request #%1 %2 Updated on %3. %4 total entries.",
+    requestError: "Request# %1 %2",
+    unexpectedResponse: "Unexpected server response."
+};
