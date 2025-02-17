5cb2fe2cc82880f8d875ced4814a84853f297bb2
Athen <cyconiexo@gmail.com>
2025-02-16 21:02:56 -0800

    small fix

--- COMP4537/labs/5/lang/messages/en/serverMsg.js
+++
@@ -1,7 +0,0 @@
-module.exports =
-{
-    notAllowed: 'DROP and DELETE statements are not allowed.',
-    methodNotAllowed: 'Method is not allowed.',
-    insertedData: 'Data inserted successfully.',
-    four04: '404 Not Found',
-};
